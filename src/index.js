5b006018e8504825ceaae3b879dce768e5f02ecc
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2021-11-09 12:27:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import list from "express-list-endpoints"
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Connected to Mongo locally")
-    app.listen(3000, () => {
-        console.log("Server is running on port 3000")
+    app.listen(process.env.PORT, () => {
+        console.log("Server is running on port " + process.env.PORT)
         console.log(list(app))
     })
 })
