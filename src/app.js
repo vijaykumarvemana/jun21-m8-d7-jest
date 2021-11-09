b5bf854785085b5558d91d6455c8de3fb0a59f59
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2021-11-09 12:36:26 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { productsRouter } from "./services/products.js";
+import { productsRouter } from "./services/products/index.js";
 
 const app = express();
 
